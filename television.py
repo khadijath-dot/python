f32fe846db6fe21b2c74136a23d2f4efce019265
khadijath-dot <khady.diaby03@gmail.com>
2026-04-08 19:24:53 -0500

    Added logic to class methods

--- television.py
+++ television.py
@@ -5,19 +5,27 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self):
-        self.status = False
-        self.muted = False
-        self.volume = Television.MIN_VOLUME
-        self.channel = Television.MIN_CHANNEL
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
 
     def power(self):
-        pass
+        self.__status = not self.__status
 
     def mute(self):
-        pass
+        if self.__status:
+            if self.__muted == True:
+                self.__muted = False
+            else:
+                self.__muted = True
 
     def channel_up(self):
-        pass
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self):
         if self.__status:
@@ -28,15 +36,22 @@ class Television:
 
     def volume_up(self):
         if self.__status:
-            self.muted = False
-            if self.__volume < Television.MIN_VOLUME:
+            self.__muted = False
+            if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
+            else:
+                self.__volume = Television.MAX_VOLUME
 
     def volume_down(self):
-        pass
+        if self.__status:
+            self.__muted = False
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
+            else:
+                self.__volume = Television.MIN_VOLUME
 
     def __str__(self):
         if self.__muted:
-            return f'Volume = {Television.MIN_VOLUME}'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = 0'
         else:
-            return f'xxx'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
